c48cf93ec01a739909a7442245f4bf0ce1da5d36
j4ger <xiayuxuan@live.com>
2021-10-23 14:40:20 +0800

    Enable fzf

--- plugins.zsh
+++ plugins.zsh
@@ -1,4 +1,9 @@
 # configure ice for loading time optimizations
-zinit light zsh-users/zsh-autosuggestions
 zinit light zsh-users/zsh-completions
-zinit light zsh-users/zsh-syntax-highlighting
+zinit light matthieusb/zsh-sdkman
+zpcompinit
+path+=("$USER_HOME/.fzf/bin")
+export PATH
+zinit light Aloxaf/fzf-tab
+zinit light zsh-users/zsh-autosuggestions
+zinit light zdharma/fast-syntax-highlighting
