249d8ec4d829fba88c8327bb7123acc5a01491f7
j4ger <xiayuxuan@live.com>
2024-02-15 20:41:39 +0800

    remove firefox and nvim related stuff

--- dot_config/nvim/lua/user/plugins/treesitter.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = { "rust","c","cpp","lua","json","toml","yaml","javascript","scala","html","python","typescript","verilog" },
-  },
-}
